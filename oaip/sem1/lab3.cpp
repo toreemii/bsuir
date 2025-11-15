45f379defd352eafae657449207edf35bef4fbb9
Вика <yavika635@gmail.com>
2025-11-15 10:20:25 +0300

    Update lab3.cpp

--- oaip/sem1/lab3.cpp
+++ oaip/sem1/lab3.cpp
@@ -1,20 +1,18 @@
 #include <iostream>  
-#include <cmath>     // Библиотека для работы с математическими функциями
+#include <cmath>     
 
 using namespace std;
 
 double M_PI = 3.14159265358979323846;
 
-// Определяем функцию для вычисления суммы S(x)
 double S(double x, int n) {
-    double sum = 0;  // Создаём переменную для хранения суммы 
+    double sum = 0;   
     for (int k = 1; k <= n; ++k) {  
         sum += pow(x, k) * cos(k * M_PI / 3) / k;  // Вычисляем и добавляем каждый элемент ряда в сумму
     }
     return sum;  
 }
 
-// Определяем функцию для вычисления Y(x)
 double Y(double x) {
     return -0.5 * log(1 - 2 * x * cos(M_PI / 3) + x * x);
 }
@@ -22,24 +20,22 @@ double Y(double x) {
 int main() {
     setlocale(LC_ALL, "Russian"); 
 
-    double x;  // Переменная для хранения значения x
+    double x;  
     cout << "Введите значение x: "; 
     cin >> x; 
 
-    double epsilon;  // Переменная для хранения значения точности epsilon
+    double epsilon;  
     cout << "Введите значение точности epsilon (например, 0.001): ";  
     cin >> epsilon;  
 
-    int n = 1;  // Начальное значение для итераций (n)
-    double Sx = 0, Yx = Y(x);  // Инициализация суммы S(x) и функции Y(x)
+    int n = 1;  
+    double Sx = 0, Yx = Y(x);  
 
-    // Цикл, который выполняется до тех пор, пока разница между S(x) и Y(x) больше epsilon
     do {
-        Sx = S(x, n);  // Вычисляем сумму S(x) для текущего n
-        ++n;  // Увеличиваем n на 1 (переходим к следующему шагу)
-    } while (fabs(Sx - Yx) >= epsilon);  // Условие выхода из цикла: |S(x) - Y(x)| < epsilon
+        Sx = S(x, n);  
+        ++n;  
+    } while (fabs(Sx - Yx) >= epsilon);  
 
-    // Выводим результат
     cout << "Требуемая точность достигнута на шаге n = " << n <<endl;
     cout << "S(x) = " << Sx << endl;
     cout << "Y(x) = " << Yx << endl;
